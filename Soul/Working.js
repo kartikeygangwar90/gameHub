b1869e356b366312cf7c2b9fef180ade39875354
Kartikey Gangwar <kartikeygangwar90@gmail.com>
2025-11-02 11:49:51 +0530

    The logic of the Stone PAper scissor is ready

--- Soul/Working.js
+++ Soul/Working.js
@@ -87,13 +87,12 @@ let O=document.querySelector("#O");
 let X=document.querySelector("#X");
 
 let btn=document.querySelectorAll(".choice");
-
 let firstMove="";
 let nextMove="";
+
 O.addEventListener("click", function (){
     firstMove="O";
     nextMove="X"
-    console.log(firstMove);
     btn.forEach(btn=>{
         btn.disabled=true;
         btn.style.backgroundColor="rgb(37, 61, 53)"
@@ -105,7 +104,6 @@ O.addEventListener("click", function (){
 X.addEventListener("click", function (){
     firstMove="X";
     nextMove="O"
-    console.log(firstMove);
     btn.forEach(btn=>{
         btn.disabled=true;
         btn.style.backgroundColor="rgb(37, 61, 53)"
@@ -115,28 +113,220 @@ X.addEventListener("click", function (){
 });
 
 btn.disabled=false
+let btns=document.querySelectorAll(".gameBox button")
+let turnDisplay=document.querySelector(".turnBox")
 function displayVal(){
-    let btns=document.querySelectorAll(".gameBox button")
+    let turnShow=document.createElement("h3");
+    turnShow.innerText=`${firstMove}'s Move`;
     let count=0;
+    turnDisplay.innerHTML="";
     btns.forEach(btn=>{
         btn.addEventListener("click", function (){
         if(count%2==0){
             btn.innerHTML=firstMove;
+            turnShow.innerText=`${nextMove}'s Move`;
+            // turnDisplay.style.marginBottom="2px";
             count++
             btn.disabled=true;    // to make sure that the value once assigned by player will not changed
             btn.style.backgroundColor="rgb(114, 151, 231)"
             btn.style.color="white"
+
         }
         else {
             btn.innerHTML=nextMove;
+            turnShow.innerText=`${firstMove}'s Move`;
             count++
             btn.disabled=true;    // to make sure that the value once assigned by player will not changed
             btn.style.backgroundColor="rgb(114, 151, 231)"
             btn.style.color="white"
+
         }
+        conclusion()
+
         })
+        turnDisplay.appendChild(turnShow);
     
     })
 
 }
 
+function conclusion(){
+    let btn1=document.getElementById("btn1")
+    let btn2=document.getElementById("btn2")
+    let btn3=document.getElementById("btn3")
+    let btn4=document.getElementById("btn4")
+    let btn5=document.getElementById("btn5")
+    let btn6=document.getElementById("btn6")
+    let btn7=document.getElementById("btn7")
+    let btn8=document.getElementById("btn8")
+    let btn9=document.getElementById("btn9")
+
+    function concDisplay(Winner,Looser){
+        btns.forEach(btn=>{
+                btn.disabled=true
+            })
+            concShow=document.createElement("h3");
+            concShow.innerText=`Cheers!! Player with move ${Winner}  wins  \n Player with the move ${Looser} Lose, Better luck next time!! `
+            turnDisplay.innerHTML=""
+            turnDisplay.appendChild(concShow);
+
+    }
+        if (btn1.innerText===`${firstMove}` && btn2.innerText===`${firstMove}` && btn3.innerText===`${firstMove}`){
+            btn1.style.backgroundColor="fuchsia";
+            btn2.style.backgroundColor="fuchsia";
+            btn3.style.backgroundColor="fuchsia";
+            btn1.style.color="Black"
+            btn2.style.color="Black"
+            btn3.style.color="Black"
+            concDisplay(firstMove,nextMove)
+        }
+        else if (btn1.innerText===`${nextMove}` && btn2.innerText===`${nextMove}` && btn3.innerText===`${nextMove}`){
+            btn1.style.backgroundColor="fuchsia";
+            btn2.style.backgroundColor="fuchsia";
+            btn3.style.backgroundColor="fuchsia";
+            btn1.style.color="Black"
+            btn2.style.color="Black"
+            btn3.style.color="Black"
+            concDisplay(nextMove,firstMove)
+        }
+        else if (btn4.innerText===`${firstMove}` && btn5.innerText===`${firstMove}` && btn6.innerText===`${firstMove}`){
+            btn4.style.backgroundColor="fuchsia";
+            btn5.style.backgroundColor="fuchsia";
+            btn6.style.backgroundColor="fuchsia";
+            btn4.style.color="Black"
+            btn5.style.color="Black"
+            btn6.style.color="Black"
+            concDisplay(firstMove,nextMove)
+        }
+        else if (btn4.innerText===`${nextMove}` && btn5.innerText===`${nextMove}` && btn6.innerText===`${nextMove}`){
+            btn4.style.backgroundColor="fuchsia";
+            btn5.style.backgroundColor="fuchsia";
+            btn6.style.backgroundColor="fuchsia";
+            btn4.style.color="Black"
+            btn5.style.color="Black"
+            btn6.style.color="Black"
+            concDisplay(nextMove,firstMove)
+        }
+        else if (btn7.innerText===`${firstMove}` && btn8.innerText===`${firstMove}` && btn9.innerText===`${firstMove}`){
+            btn7.style.backgroundColor="fuchsia";
+            btn8.style.backgroundColor="fuchsia";
+            btn9.style.backgroundColor="fuchsia";
+            btn7.style.color="Black"
+            btn8.style.color="Black"
+            btn9.style.color="Black"
+            concDisplay(firstMove,nextMove)
+        }
+        else if (btn7.innerText===`${nextMove}` && btn8.innerText===`${nextMove}` && btn9.innerText===`${nextMove}`){
+            btn7.style.backgroundColor="fuchsia";
+            btn8.style.backgroundColor="fuchsia";
+            btn9.style.backgroundColor="fuchsia";
+            btn7.style.color="Black"
+            btn8.style.color="Black"
+            btn9.style.color="Black"
+            concDisplay(nextMove,firstMove)
+        }
+        else if (btn1.innerText===`${firstMove}` && btn4.innerText===`${firstMove}` && btn7.innerText===`${firstMove}`){
+            btn1.style.backgroundColor="fuchsia";
+            btn4.style.backgroundColor="fuchsia";
+            btn7.style.backgroundColor="fuchsia";
+            btn1.style.color="Black"
+            btn4.style.color="Black"
+            btn7.style.color="Black"
+            concDisplay(firstMove,nextMove)
+        }
+        else if (btn1.innerText===`${nextMove}` && btn4.innerText===`${nextMove}` && btn7.innerText===`${nextMove}`){
+            btn1.style.backgroundColor="fuchsia";
+            btn4.style.backgroundColor="fuchsia";
+            btn7.style.backgroundColor="fuchsia";
+            btn1.style.color="Black"
+            btn4.style.color="Black"
+            btn7.style.color="Black"
+            concDisplay(nextMove,firstMove)
+        }
+        else if (btn2.innerText===`${firstMove}` && btn5.innerText===`${firstMove}` && btn8.innerText===`${firstMove}`){
+            btn2.style.backgroundColor="fuchsia";
+            btn5.style.backgroundColor="fuchsia";
+            btn8.style.backgroundColor="fuchsia";
+            btn2.style.color="Black"
+            btn5.style.color="Black"
+            btn8.style.color="Black"
+            concDisplay(firstMove,nextMove)
+        }
+        else if (btn2.innerText===`${nextMove}` && btn5.innerText===`${nextMove}` && btn8.innerText===`${nextMove}`){
+            btn2.style.backgroundColor="fuchsia";
+            btn5.style.backgroundColor="fuchsia";
+            btn8.style.backgroundColor="fuchsia";
+            btn2.style.color="Black"
+            btn5.style.color="Black"
+            btn8.style.color="Black"
+            concDisplay(nextMove,firstMove)
+        }
+        else if (btn3.innerText===`${firstMove}` && btn6.innerText===`${firstMove}` && btn9.innerText===`${firstMove}`){
+            btn3.style.backgroundColor="fuchsia";
+            btn6.style.backgroundColor="fuchsia";
+            btn9.style.backgroundColor="fuchsia";
+            btn3.style.color="Black"
+            btn6.style.color="Black"
+            btn9.style.color="Black"
+            concDisplay(firstMove,nextMove)
+        }
+        else if (btn3.innerText===`${nextMove}` && btn6.innerText===`${nextMove}` && btn9.innerText===`${nextMove}`){
+            btn3.style.backgroundColor="fuchsia";
+            btn6.style.backgroundColor="fuchsia";
+            btn9.style.backgroundColor="fuchsia";
+            btn3.style.color="Black"
+            btn6.style.color="Black"
+            btn9.style.color="Black"
+            concDisplay(nextMove,firstMove)
+        }
+        else if (btn1.innerText===`${firstMove}` && btn5.innerText===`${firstMove}` && btn9.innerText===`${firstMove}`){
+            btn1.style.backgroundColor="fuchsia";
+            btn5.style.backgroundColor="fuchsia";
+            btn9.style.backgroundColor="fuchsia";
+            btn1.style.color="Black"
+            btn5.style.color="Black"
+            btn9.style.color="Black"
+            concDisplay(firstMove,nextMove)
+        }
+        else if (btn1.innerText===`${nextMove}` && btn5.innerText===`${nextMove}` && btn9.innerText===`${nextMove}`){
+            btn1.style.backgroundColor="fuchsia";
+            btn5.style.backgroundColor="fuchsia";
+            btn9.style.backgroundColor="fuchsia";
+            btn1.style.color="Black"
+            btn5.style.color="Black"
+            btn9.style.color="Black"
+            concDisplay(nextMove,firstMove)
+        }
+        else if (btn3.innerText===`${firstMove}` && btn5.innerText===`${firstMove}` && btn7.innerText===`${firstMove}`){
+            btn3.style.backgroundColor="fuchsia";
+            btn5.style.backgroundColor="fuchsia";
+            btn7.style.backgroundColor="fuchsia";
+            btn3.style.color="Black"
+            btn5.style.color="Black"
+            btn7.style.color="Black"
+            concDisplay(firstMove,nextMove)
+        }
+        else if (btn3.innerText===`${nextMove}` && btn5.innerText===`${nextMove}` && btn7.innerText===`${nextMove}`){
+            btn3.style.backgroundColor="fuchsia";
+            btn5.style.backgroundColor="fuchsia";
+            btn7.style.backgroundColor="fuchsia";
+            btn3.style.color="Black"
+            btn5.style.color="Black"
+            btn7.style.color="Black"
+            concDisplay(nextMove,firstMove)
+        }
+
+        
+    }
+    
+    let resetGame=document.querySelector(".reset2")
+    resetGame.addEventListener("click", function (){
+        firstMove=""
+        nextMove=""
+        btns.forEach(btn => {
+        btn.innerText=""
+        btn.disabled = true;
+        btn.style.backgroundColor = "";
+        btn.style.color = "";
+    });
+    })
